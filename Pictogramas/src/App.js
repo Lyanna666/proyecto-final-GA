851112b38380c5d1a84155cfecc75a1181b29587
jacobo87 <jacobo@MacBook-Air-de-Jacobo.local>
2022-06-24 16:53:59 +0200

    Creado login sin terminar

--- Pictogramas/src/App.js
+++ Pictogramas/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Detail from './pages/Detail';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Detail from "./pages/Detail";
 
-import * as constantsSpanish from './Constants/spanish';
-import * as constantsEnglish from './Constants/english';
+import * as constantsSpanish from "./Constants/spanish";
+import * as constantsEnglish from "./Constants/english";
 
 const App = () => {
   const [language, setLanguage] = useState(constantsSpanish);
@@ -27,7 +27,7 @@ const App = () => {
         path="/"
         element={<Home language={language} changeLanguage={changeLanguage} />}
       />
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login language={language} />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/dashboard/:id" element={<Detail />} />
     </Routes>
--- Pictogramas/src/pages/Login.jsx
+++ Pictogramas/src/pages/Login.jsx
@@ -1,15 +1,62 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import CustomButton from "../components/elements/customButton";
+import "../styles/login.css";
 
 // import styled from 'styled-components'; Si quereís usar esto lo descomentamos
 
-import Header from '../components/header';
-
-const Login = () => {
+import Header from "../components/header";
+const Login = (props) => {
   return (
     <>
       <Header />
-      <h1>Aquí iría el Login / Registro</h1>
-      <Link to={'/dashboard'}>Iniciar sesion</Link>
+      <div className="login-container">
+        <div className="container">
+          <div className="login">
+            <h1> {props.title} </h1>
+            <p>{props.language.LOGIN_TITLE}</p>
+            <form>
+              <label>
+                {props.language.LOGIN_EMAIL}:
+                <input type="text" name="username" />
+              </label>
+              <label>
+                {props.language.LOGIN_PASSWORD}:
+                <input type="password" name="password" />
+              </label>
+              <CustomButton
+                color="blue"
+                name={props.language.LOGIN_BUTTON}
+                size="small"
+                /* onClick={props.changeLanguage} */
+              />
+            </form>
+          </div>
+          <p>{props.language.LOGIN_SOCIAL}</p>
+          <div className="social">
+            <div>
+              <ul>
+                <li>
+                  <a href="/">Facebook</a>
+                </li>
+                <li>
+                  <a href="/">Twitter</a>
+                </li>
+                <li>
+                  <a href="/">Instagram</a>
+                </li>
+                <li>
+                  <a href="/">Google</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="terms">
+          <p> {props.language.LOGIN_TERMS} </p>
+          <p> {props.language.LOGIN_TERMS_LINK} </p>
+          <Link to={"/"}> {props.language.HEADER_NAV_HOME} </Link>
+        </div>
+      </div>
     </>
   );
 };
