c4a8c91ee6c30428b4e0eb8f63e1933d4a94a9b5
Andrea Gonzalez <ggarciaandrea@gmail.com>
2022-06-26 01:02:12 +0200

    correción en el menu del header

--- Pictogramas/src/components/Header/header.jsx
+++ Pictogramas/src/components/Header/header.jsx
@@ -9,7 +9,20 @@ import HeaderMenu from '../HeaderMenu/header-menu';
 const Header = () => {
   const context = useContext(AppContext);
 
+  // Se añade un evento que se ejecuta cuando el tamaño de la pantalla cambia
+  window.addEventListener('resize', checkForWindowResize);
+
+  function checkForWindowResize() {
+    //Si el tamaño del navegador es menos de 1024 escondo el menu para que no quede abierto al hacer resize
+    if (window.innerWidth > 1024) {
+      setOpenedMenu(false);
+    }
+  }
+
   const [openedMenu, setOpenedMenu] = useState(false);
+
+  //Creo un estado para saber si se está mostrando el menú o no.
+  // En el evento onClick cambio ese estado a true o false en función de si se muestra el menú
 
   const onClickMenu = event => {
     if (openedMenu) {
@@ -87,6 +100,7 @@ const Header = () => {
           Menu
         </button>
       </header>
+      {/* Si el estado del menú es true lo muestro, si no, no */}
       {openedMenu ? <HeaderMenu /> : <></>}
     </>
   );
