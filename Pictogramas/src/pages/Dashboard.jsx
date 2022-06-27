eb30c80af36152dc21cd9c202288be1336025142
Andrea Gonzalez <ggarciaandrea@gmail.com>
2022-06-27 19:55:14 +0200

    errore corregidos

--- Pictogramas/src/pages/Dashboard.jsx
+++ Pictogramas/src/pages/Dashboard.jsx
@@ -1,17 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
-<<<<<<< HEAD
-import Header from "../components/Header/header";
-import Footer from "../components/Footer/footer";
-import Pictograma from "../components/pictograma";
-import Aside from "../components/aside/aside";
-=======
 import Header from '../components/Header/header';
-import Pictograma from '../components/pictograma';
+import Footer from '../components/Footer/footer';
 import Pictograms from '../components/Pictograms/pictograms';
->>>>>>> a7f673ee1298388ce2cf227cae9d91678c9d6d8e
+import Aside from '../components/aside/aside';
 
 const Dashboard = () => {
   const Dashboardmain = styled.main`
@@ -24,33 +18,12 @@ const Dashboard = () => {
   return (
     <>
       <Header />
-<<<<<<< HEAD
       <Aside />
       <Dashboardmain>
         <h1>Esto es el dashboard de usuario</h1>
-        <Pictograma
-          info={{
-            url: "https://static.arasaac.org/pictograms/2349/2349_action-past_300.png",
-            name: "comer",
-            id: "comer",
-          }}
-        />
+        <Pictograms />
       </Dashboardmain>
       <Footer />
-=======
-      <section className="dashboard-section">
-        <h1>Esto es el dashboard de usuario</h1>
-        <Pictograms />
-        {/* <Pictograma
-        info={{
-          url:
-            'https://static.arasaac.org/pictograms/2349/2349_action-past_300.png',
-          name: 'comer',
-          id: 'comer',
-        }}
-      /> */}
-      </section>
->>>>>>> a7f673ee1298388ce2cf227cae9d91678c9d6d8e
     </>
   );
 };
