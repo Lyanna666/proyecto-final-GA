7b385fe9132adfad435e70d377864d6aa8d3723c
Jacobo Azmani <azmanijacobo@gmail.com>
2022-06-27 19:16:43 +0200

    Corregido aside y contenido de dashboard, fondo y estilos

--- Pictogramas/src/pages/Dashboard.jsx
+++ Pictogramas/src/pages/Dashboard.jsx
@@ -1,47 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
-<<<<<<< HEAD
-import Header from "../components/Header/header";
-import Footer from "../components/Footer/footer";
-import Pictograma from "../components/pictograma";
-import Aside from "../components/aside/aside";
-=======
 import Header from '../components/Header/header';
+import Footer from '../components/Footer/footer';
 import Pictograma from '../components/pictograma';
+import Aside from '../components/aside/aside';
 import Pictograms from '../components/Pictograms/pictograms';
->>>>>>> a7f673ee1298388ce2cf227cae9d91678c9d6d8e
 
 const Dashboard = () => {
   const Dashboardmain = styled.main`
     display: flex;
     flex-direction: column;
+    width: 90%;
     height: 100%;
     min-height: 150vh;
+    background-color: #eaeaea;
+  `;
+
+  const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    min-height: 150vh;
+    background-color: #eaeaea;
+    width: 100%;
+    padding: 0;
+    margin: 0 auto;
   `;
 
   return (
     <>
       <Header />
-<<<<<<< HEAD
-      <Aside />
-      <Dashboardmain>
-        <h1>Esto es el dashboard de usuario</h1>
-        <Pictograma
-          info={{
-            url: "https://static.arasaac.org/pictograms/2349/2349_action-past_300.png",
-            name: "comer",
-            id: "comer",
-          }}
-        />
-      </Dashboardmain>
-      <Footer />
-=======
-      <section className="dashboard-section">
-        <h1>Esto es el dashboard de usuario</h1>
-        <Pictograms />
-        {/* <Pictograma
+      <Container>
+        <Aside />
+        <Dashboardmain>
+          <section className="dashboard-section">
+            <h1>Esto es el dashboard de usuario</h1>
+            <Pictograms />
+            {/* <Pictograma
         info={{
           url:
             'https://static.arasaac.org/pictograms/2349/2349_action-past_300.png',
@@ -49,8 +46,10 @@ const Dashboard = () => {
           id: 'comer',
         }}
       /> */}
-      </section>
->>>>>>> a7f673ee1298388ce2cf227cae9d91678c9d6d8e
+          </section>
+        </Dashboardmain>
+      </Container>
+      <Footer />
     </>
   );
 };
