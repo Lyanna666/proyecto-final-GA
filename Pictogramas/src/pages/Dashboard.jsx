d7da91472a1a3bfef6aa56b7a0d67246a63b5e3b
jacobo87 <jacobo@MacBook-Air-de-Jacobo.local>
2022-06-27 12:52:51 +0200

    Corregido aside sin terminar

--- Pictogramas/src/pages/Dashboard.jsx
+++ Pictogramas/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-// import styled from 'styled-components'; Si quereís usar esto lo descomentamos
+import styled from "styled-components";
 
 import Header from "../components/Header/header";
 import Footer from "../components/Footer/footer";
@@ -8,18 +8,27 @@ import Pictograma from "../components/pictograma";
 import Aside from "../components/aside/aside";
 
 const Dashboard = () => {
+  const Dashboardmain = styled.main`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    min-height: 150vh;
+  `;
+
   return (
     <>
       <Header />
       <Aside />
-      <h1>Esto es el dashboard de usuario</h1>
-      <Pictograma
-        info={{
-          url: "https://static.arasaac.org/pictograms/2349/2349_action-past_300.png",
-          name: "comer",
-          id: "comer",
-        }}
-      />
+      <Dashboardmain>
+        <h1>Esto es el dashboard de usuario</h1>
+        <Pictograma
+          info={{
+            url: "https://static.arasaac.org/pictograms/2349/2349_action-past_300.png",
+            name: "comer",
+            id: "comer",
+          }}
+        />
+      </Dashboardmain>
       <Footer />
     </>
   );
