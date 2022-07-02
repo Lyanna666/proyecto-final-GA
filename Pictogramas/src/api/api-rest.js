310b09012022944c1521d53783e1d637b5f87161
Andrea Gonzalez <ggarciaandrea@gmail.com>
2022-07-02 12:29:45 +0200

    merge con rana jacobo

--- Pictogramas/src/api/api-rest.js
+++ Pictogramas/src/api/api-rest.js
@@ -1,13 +1,17 @@
 // API REST
 // ------------------------------------------------------------
 
-const urlAll = 'https://api.arasaac.org/api/pictograms/all/';
+const urlGlobal = 'https://api.arasaac.org/api/pictograms';
+
+const urlAll = '/all/';
+const urlBest = '/bestsearch/';
+const urlSearch = '/search/';
 
 export async function fetchAllPictograms(language) {
-  console.log(urlAll + language);
+  console.log(urlGlobal + urlAll + language);
 
   try {
-    const response = await fetch(urlAll + language, {
+    const response = await fetch(urlGlobal + urlAll + language, {
       method: 'GET',
     });
     const pictograms = await response.json();
@@ -18,4 +22,17 @@ export async function fetchAllPictograms(language) {
   }
 }
 
-export default fetchAllPictograms;
+export async function fetchAllPictogramsBySearch(search, language) {
+  /* console.log(urlAll + language); */
+
+  try {
+    const response = await fetch(urlGlobal + language + urlSearch + search, {
+      method: 'GET',
+    });
+    const pictograms = await response.json();
+    return pictograms;
+  } catch (error) {
+    console.error({ error });
+    alert('Ha ocurrido un error:', error);
+  }
+}
--- Pictogramas/src/components/Pictograms/pictograms.jsx
+++ Pictogramas/src/components/Pictograms/pictograms.jsx
@@ -1,7 +1,10 @@
 import './pictograms.css';
 
 import { useContext, React, useState, useEffect } from 'react';
-import fetchAllPictograms from '../../api/api-rest';
+import {
+  fetchAllPictograms,
+  fetchAllPictogramsBySearch,
+} from '../../api/api-rest';
 import Pagination from '../pagination/pagination';
 import Posts from './pictogram';
 import AppContext from '../../AppContext';
@@ -10,19 +13,21 @@ import Spinner from '../loading/loading';
 const Pictograms = () => {
   const context = useContext(AppContext);
   const [items, setItems] = useState([]);
+  const [pictograms, setPictograms] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [keyword, setKeyword] = useState('');
 
   function getAllPictograms() {
     setLoading(true);
-    console.log(context.language.LANGUAGE);
+    // console.log(context.language.LANGUAGE);
     fetchAllPictograms(context.language.LANGUAGE)
       .then(data => {
         setItems(data);
-        console.log(data.length);
+        setPictograms(data);
         setLoading(false);
       })
-      .catch(e => {
-        console.error(e);
+      .catch(error => {
+        console.error(error);
         setLoading(false);
       });
   }
@@ -36,9 +41,36 @@ const Pictograms = () => {
     getAllPictograms();
   }, []);
 
-  function onChangeSearchInput(event) {
+  /*   function onChangeSearchInput(event) {
     console.log(event.target.value);
-  }
+  } */
+
+  // -------------- Búsqueda de pictogramas -----------------
+  const getFilteredPictograms = ({ search }) => {
+    const searchString = new RegExp(search, 'i');
+    // console.log('******************', pictograms);
+    const filteredPictograms = items.filter(pictogram => {
+      console.log(pictogram.keywords[0].keyword);
+      return searchString.test(pictogram.keywords[0].keyword);
+    });
+    console.log(filteredPictograms);
+    return filteredPictograms;
+  };
+
+  const handleKeyUp = event => {
+    const value = event.target.value;
+    setKeyword(value);
+    const filteredPictograms = getFilteredPictograms({
+      keyword,
+      search: value,
+    });
+    setPictograms(filteredPictograms);
+  };
+
+  // -----------------------------------------------------
+  // ----------------- Búsqueda de pictogramas por palabra -----------------------
+  /* const fetchAllPictogramsBySearch  */
+  // -----------------------------------------------------
 
   return (
     <>
@@ -66,14 +98,19 @@ const Pictograms = () => {
           <input
             type="search"
             placeholder={context.language.DASHBOARD_SEARCH}
-            onKeyUp={onChangeSearchInput}
+            onKeyUp={handleKeyUp}
+            id="search"
           />
+          <button type="submit" id="buscar">
+            buscar
+          </button>
         </form>
         <div>
-          {items.length > 0 ? (
+          {pictograms.length > 0 ? (
             <>
               <Pagination
-                data={items}
+                data={pictograms}
+                numberPages={pictograms.length}
                 RenderComponent={Posts}
                 title="Pictogramas Paginación"
                 buttonConst={3}
