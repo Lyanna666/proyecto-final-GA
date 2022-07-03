01dfeb01cfa04b2a9ef740d557707f544e652c87
Andrea Gonzalez <ggarciaandrea@gmail.com>
2022-07-03 19:24:30 +0200

    cambio en custom button y en detail

--- Pictogramas/src/pages/Detail.jsx
+++ Pictogramas/src/pages/Detail.jsx
@@ -1,17 +1,35 @@
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
-// import styled from 'styled-components'; Si quereís usar esto lo descomentamos
-
+import styled from 'styled-components';
 import Header from '../components/Header/header';
+import Aside from '../components/aside/aside';
 
 const Detail = () => {
   let { id } = useParams();
 
+  const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    min-height: 150vh;
+    background-color: #eaeaea;
+    width: 100%;
+    padding: 1rem;
+    margin: 0 auto;
+
+    @media (max-width: 768px) {
+      flex-direction: column;
+    }
+  `;
+
   return (
     <>
       <Header />
-      <h1>Esto es el detalle del pictograma {id}</h1>
+      <Container>
+        <Aside />
+        <h1>Esto es el detalle del pictograma {id}</h1>
+      </Container>
     </>
   );
 };
