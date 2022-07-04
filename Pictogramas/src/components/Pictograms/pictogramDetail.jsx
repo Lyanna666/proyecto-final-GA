08f050531c40a4ec2d67439e4868479afef0a6ff
Andrea Gonzalez <ggarciaandrea@gmail.com>
2022-07-04 19:35:06 +0200

    trabajando en el detalle de pictogramas

--- Pictogramas/src/components/Pictograms/pictogramDetail.jsx
+++ Pictogramas/src/components/Pictograms/pictogramDetail.jsx
@@ -1,8 +1,10 @@
 import { Link, useParams } from 'react-router-dom';
 import AppContext from '../../AppContext';
 
+import styled from 'styled-components';
 import { useContext, React, useState, useEffect } from 'react';
 import { fetchPictogramById } from '../../api/api-rest';
+import CustomButton from '../elements/customButton';
 import Spinner from '../loading/loading';
 
 import './pictogramDetail.css';
@@ -39,11 +41,12 @@ const PictogramDetail = props => {
     <>
       {loading ? <Spinner allWindow={true} /> : <></>}
       <div className="pictograma">
-        <h2>Pictograma {params.id}</h2>
+        <h1>Pictograma {params.id}</h1>
         {pictogramInfo != null ? (
-          <div className="pictograma-img">
-            <div>
-              <h1>{pictogramInfo.keywords[0].keyword}</h1>
+          <ContentPictogram>
+            <DivPictogram>
+              <H2Pictogram>{pictogramInfo.keywords[0].keyword} ▶</H2Pictogram>
+
               <picture>
                 <img
                   src={`https://static.arasaac.org/pictograms/${
@@ -53,11 +56,28 @@ const PictogramDetail = props => {
                 />
               </picture>
               <div>
-                <button type="button">Descargar</button>
-                <button type="button">Imprimir</button>
+                <CustomButton name="Descargar" color="green" size="small" />
+                <CustomButton name="Imprimir" color="green" size="small" />
+                <CustomButton name="Descargar" color="green" size="small" />
               </div>
+            </DivPictogram>
+            <div>
+              <ul>
+                {pictogramInfo.keywords.map((information, index) => (
+                  <li key={index}>
+                    <h3>{information.keyword.toUpperCase()}</h3>
+                    <p>{information.meaning}</p>
+                  </li>
+                ))}
+              </ul>
+
+              <ul>
+                {pictogramInfo.tags.map((tag, index) => (
+                  <LiCategory key={tag}>{tag}</LiCategory>
+                ))}
+              </ul>
             </div>
-          </div>
+          </ContentPictogram>
         ) : (
           <></>
         )}
@@ -65,5 +85,47 @@ const PictogramDetail = props => {
     </>
   );
 };
+
+const DivPictogram = styled.div`
+  display: flex;
+  width: 30%;
+  text-transform: uppercase;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(255, 255, 255);
+  border-radius: 2px;
+  margin: 1rem;
+  box-shadow: rgb(0 0 0 / 12%) 0px 1px 6px, rgb(0 0 0 / 12%) 0px 1px 4px;
+
+  @media (max-width: 768px) {
+  }
+`;
+
+const LiCategory = styled.li`
+  margin: 0.2rem;
+  background-color: rgb(229, 229, 229);
+  padding: 0.5rem;
+  border-radius: 1rem;
+`;
+
+const ContentPictogram = styled.div`
+  padding: 0.5rem;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+const H2Pictogram = styled.h2`
+  display: block;
+  padding: 0.5rem;
+  width: 100%;
+  font-weight: 400;
+  color: var(--gray);
+  margin: 0;
+  margin-bottom: 1rem;
+  text-align: center;
+  background-color: #f5f5f5;
+`;
 
 export default PictogramDetail;
