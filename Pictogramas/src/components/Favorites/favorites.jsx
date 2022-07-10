7b16f3996a99c478809b7a05b0b982d299f026d7
Jacobo Azmani <azmanijacobo@gmail.com>
2022-07-10 15:37:25 +0200

    Add styled a favoritos

--- Pictogramas/src/components/Favorites/favorites.jsx
+++ Pictogramas/src/components/Favorites/favorites.jsx
@@ -14,32 +14,53 @@ const Favorites = (props) => {
     justify-content: flex-start;
     margin-top: 2rem;
     margin-bottom: 2rem;
+    border: 1px solid #e0e0e0;
+    background-color: #fafafa;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
   `;
 
-  const list = styled.ul`
+  const H2 = styled.h2`
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin-bottom: 1rem;
+  `;
+
+  const ContenedorFavoritos = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  `;
+
+  const List = styled.ul`
     list-style: none;
     padding: 0;
     margin: 0;
   `;
 
-  const listItem = styled.li`
+  const ListItem = styled.li`
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-between;
+    justify-content: flex-start;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
   `;
 
   return (
     <>
       <MostrarFavoritos>
-        <h2>{context.language.FAVO_TITLE}</h2>
+        <H2>{context.language.FAVO_TITLE}</H2>
         {localStorage.getItem("favorites") && (
-          <div>
-            <list>
-              ID:
-              <listItem>{localStorage.getItem("favorites")}</listItem>
-            </list>
-          </div>
+          <ContenedorFavoritos>
+            <List>
+              <ListItem> ID: {localStorage.getItem("favorites")}</ListItem>
+            </List>
+          </ContenedorFavoritos>
         )}
       </MostrarFavoritos>
     </>
